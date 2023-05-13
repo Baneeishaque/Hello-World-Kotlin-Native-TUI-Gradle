e4daf759ef66169bae7e8c564224b23dd7954e52
Banee Ishaque K <baneeishaque@gmail.com>
2023-05-13 18:57:21 +0530

    Standard Project Structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,16 +13,16 @@ kotlin {
     val hostOs = System.getProperty("os.name")
     val isArm64 = System.getProperty("os.arch") == "aarch64"
     val isMingwX64 = hostOs.startsWith("Windows")
-    val mingwX64Target = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("mingwX64")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("mingwX64")
-        hostOs == "Linux" && isArm64 -> linuxArm64("mingwX64")
-        hostOs == "Linux" && !isArm64 -> linuxX64("mingwX64")
-        isMingwX64 -> mingwX64("mingwX64")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
-    mingwX64Target.apply {
+    nativeTarget.apply {
         binaries {
             executable {
                 entryPoint = "main"
@@ -30,13 +30,7 @@ kotlin {
         }
     }
     sourceSets {
-        val mingwX64Main by getting
-        val mingwX64Test by getting
-    }
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(20))
+        val nativeMain by getting
+        val nativeTest by getting
     }
 }
